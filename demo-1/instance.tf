94fb3ce1c1844fc0acbdb5eeb93ff2c48b5ff6e5
Alex Averbuch <averbuch.tech@gmail.com>
2023-08-01 15:39:33 +0300

    added 1 to example

--- demo-1/instance.tf
+++ demo-1/instance.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "example" {
+resource "aws_instance" "example1" {
   ami           = lookup(var.AMIS, var.AWS_REGION, "") # last parameter is the default value
   instance_type = "t2.micro"
 }
